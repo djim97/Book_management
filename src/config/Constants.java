17df41decd09716b97911c8d5067a241492479f6
Djimouna Bacary BADJI <dbadjidk@groupeisi.com>
2026-05-24 20:30:30 +0000

    Update Constants.java

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -2,7 +2,7 @@ package config;
 
 public class Constants {
     public static final String USER     = "postgres";
-    public static final String PASSWORD = "08Octobre#";  // use env var ideally
+    public static final String PASSWORD = "your_password";  // use env var ideally
     public static final String DATABASE = "book_db";
     public static final int    PORT     = 5432;
     public static final String URL      = "jdbc:postgresql://localhost:" + PORT + "/" + DATABASE;
